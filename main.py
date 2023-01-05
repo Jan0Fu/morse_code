45f82f4de58c4b8ef294b21fb8c6d4a6a97c5030
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-01-05 16:59:58 +0100

    Refactor code and logo addition

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
-                    'C': '-.-.', 'D': '-..', 'E': '.',
-                    'F': '..-.', 'G': '--.', 'H': '....',
-                    'I': '..', 'J': '.---', 'K': '-.-',
-                    'L': '.-..', 'M': '--', 'N': '-.',
-                    'O': '---', 'P': '.--.', 'Q': '--.-',
-                    'R': '.-.', 'S': '...', 'T': '-',
-                    'U': '..-', 'V': '...-', 'W': '.--',
-                    'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                    '1': '.----', '2': '..---', '3': '...--',
-                    '4': '....-', '5': '.....', '6': '-....',
-                    '7': '--...', '8': '---..', '9': '----.',
-                    '0': '-----', ', ': '--..--', '.': '.-.-.-',
-                    '?': '..--..', '/': '-..-.', '-': '-....-',
-                    '(': '-.--.', ')': '-.--.-'}
+from code import morse_code
+from art import logo
 
+print(logo)
 user_input = input("Translate to Morse Code: ").upper()
-print(user_input)
 
 result = []
 
 
 def cipher():
-    for char in user_input:
-        result.append(MORSE_CODE_DICT[char])
+    try:
+        for char in user_input:
+
+            result.append(morse_code[char])
+
+    except KeyError:
+        print("Error, invalid input")
 
 
 cipher()
-print(result)
+print(' '.join(result))
